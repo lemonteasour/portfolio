12745a72452a94b79b8c87c4b16d185a647f2158
jay <29498758+jayhuich@users.noreply.github.com>
2025-08-03 14:17:11 +0900

    fix: Footer border

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="py-8">
+    <footer className="mx-6 py-8">
       <div className="container mx-auto flex justify-center items-center border-t border-gray-200 dark:border-gray-600 p-6">
         <p className="text-gray-500">
           &copy; 2025 <a href="https://github.com/jayhuich">@jayhuich</a>
